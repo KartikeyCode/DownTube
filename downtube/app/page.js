b333c137ce3fd74c7963d33a6dcc102827e9e564
KartikeyCode <codekartikey@gmail.com>
2024-01-27 14:41:41 +0530

    add change

--- downtube/app/page.js
+++ downtube/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/download', {
+      const response = await fetch('https://down-tube-pi.vercel.app//download', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
